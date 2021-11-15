ae45ccd791845d4323f80a0530ebd8cff49fc4fe
Arınç Alp Eren <40849529+alpekin98@users.noreply.github.com>
2021-11-15 14:03:47 +0300

    Update printArtifactPaths.groovy

--- vars/printArtifactPaths.groovy
+++ vars/printArtifactPaths.groovy
@@ -3,6 +3,14 @@ def call(Map pathDictionary){
         echo "Artifact Type = " + k
         for(def i=0;i<v.size();i++){
             echo "Location: " + v[i]
+            pushToArtifactory(v[i])
         }
     }
+}
+
+def pushToArtifactory(String artifactPath){
+    def splitted = artifactPath.split("/")
+    def artifactName = splitted[splitted.length-1]
+    def artifactType = splitted[splitted.length-2]
+    sh "curl -u arinc.alp.98@gmail.com:AP7y8ekbLckRdzX7RZYYFbU717x -XPUT 'https://alpekin98.jfrog.io/artifactory/my-test-debian/pool/${artifactType}/${artifactName};deb.distribution=latest;deb.component=main;deb.architecture=amd64' -T ${artifactPath}"
 }
