e0409f134eca45fb8a944df049fab68a9eef6b16
Arınç Alp Eren <40849529+alpekin98@users.noreply.github.com>
2021-11-15 11:26:02 +0300

    Update printArtifactPaths.groovy

--- vars/printArtifactPaths.groovy
+++ vars/printArtifactPaths.groovy
@@ -1,6 +1,5 @@
-def call(def pathDictionary){
-    def pathDictionaryMap = (Map) pathDictionary
-    pathDictionaryMap.each{ k, v ->
+def call(Map pathDictionary){
+    pathDictionary.each{ k, v ->
         echo "Artifact Type = " + k
         for(def i=0;i<v.size();i++){
             echo "Location: " + v[i]
