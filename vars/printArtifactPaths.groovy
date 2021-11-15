99ea819511e6fecdc198f9b358d32c1494a7245c
Arınç Alp Eren <40849529+alpekin98@users.noreply.github.com>
2021-11-15 15:01:11 +0300

    ENV vars added

--- vars/printArtifactPaths.groovy
+++ vars/printArtifactPaths.groovy
@@ -3,14 +3,13 @@ def call(Map pathDictionary){
         echo "Artifact Type = " + k
         for(def i=0;i<v.size();i++){
             echo "Location: " + v[i]
-            pushToArtifactory(v[i])
+            pushToArtifactory(v[i],k)
         }
     }
 }
 
-def pushToArtifactory(String artifactPath){
+def pushToArtifactory(String artifactPath, String artifactType){
     def splitted = artifactPath.split("/")
     def artifactName = splitted[splitted.length-1]
-    def artifactType = splitted[splitted.length-2]
-    sh "curl -u arinc.alp.98@gmail.com:AP7y8ekbLckRdzX7RZYYFbU717x -XPUT 'https://alpekin98.jfrog.io/artifactory/my-test-debian/pool/${artifactType}/${artifactName};deb.distribution=latest;deb.component=main;deb.architecture=amd64' -T ${artifactPath}"
+    sh "curl -u ${ARTIFACTORY_CREDENTIALS} -XPUT '${ARTIFACTORY_URL}/${artifactType}/${artifactName}' -T ${artifactPath}"
 }
